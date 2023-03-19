08fe42ca59d868e37c7d74363603bf770517b6d9
Hazman <hazman0104@gmail.com>
2023-03-19 13:02:40 +0700

    3

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // api url
-const api_url = "https://api.thingspeak.com/channels/2071246/feeds.json?api_key=34RRS9WJRTR5PJZU&results=1";
+const api_url = "https://api.thingspeak.com/channels/2071475/feeds.json?api_key=X85TVSZ5J1RFMEPX&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -13,11 +13,8 @@ setInterval(() => {
         document.getElementById("Motion").innerHTML = "Motion Detected";
       }
       document.getElementById("Switch").innerHTML = obj.field1;
-      if (obj.field1 == 1) {
+      if (obj.field1 >= 6000) {
         document.getElementById("Switch").innerHTML = "ON";
-      }
-      else {
-        document.getElementById("Switch").innerHTML = "OFF";
       }
     });
 }, 1000);
