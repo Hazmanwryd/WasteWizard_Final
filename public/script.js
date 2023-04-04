12db151ac6fba7ecf297b64c969cb7a7720bdc5c
Hazman <hazman0104@gmail.com>
2023-04-04 22:53:59 +0700

    a

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-//loading bar code?
-const api_url = "https://api.thingspeak.com/channels/2071475/feeds.json?api_key=X85TVSZ5J1RFMEPX&results=1";
+// baterai bar html?
+const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -8,12 +8,7 @@ setInterval(() => {
     .then((res) => {
       var field = JSON.stringify(res.feeds[0]);
       var obj = JSON.parse(field);
-      if (obj.field2 == 1) {
-        document.getElementById("Motion").innerHTML = "Motion Detected";
-      }
-      if (obj.field1 >= 6000) {
-        document.getElementById("Switch").innerHTML = "ON";
-      }
-      }
-    );
+      document.getElementById("Switch").innerHTML = obj.field1;
+      document.getElementById("Motion").innerHTML = obj.field2;
+    });
 }, 1000);
