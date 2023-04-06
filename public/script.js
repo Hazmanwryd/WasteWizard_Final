44d8c54f6616b7907813b317e7817da8c19b7d34
Hazman <hazman0104@gmail.com>
2023-04-06 12:39:14 +0700

    1

--- public/script.js
+++ public/script.js
@@ -1,14 +1,11 @@
-// baterai bar html?
-const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
-// masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
+const api_url = "https://sgp1.blynk.cloud/external/api/get?token=pfBF6YDvzUjmSHQfZXm05yLloNFIZ5AJ&v4&v6&v7";
 
 setInterval(() => {
-  fetch(api_url)
-    .then((hasil) => hasil.json())
-    .then((res) => {
-      var field = JSON.stringify(res.feeds[0]);
-      var obj = JSON.parse(field);
-      document.getElementById("Switch").innerHTML = obj.field1;
-      document.getElementById("Motion").innerHTML = obj.field2;
-    });
+fetch(api_url)
+  .then((hasil) => hasil.json())
+  .then((res) => {
+    var field = JSON.stringify(res.v4);
+    var obj = JSON.parse(field);
+    document.getElementById("v4").innerHTML = obj;
+  });
 }, 1000);
